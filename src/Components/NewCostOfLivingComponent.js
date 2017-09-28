a8aaded287177a10c57f22b8c97ef566caeb6a20
Lonneke <l.vuur@outlook.com>
2017-09-28 22:29:09 +0200

    FIN Refactor NewCOL, extract RandomPosition

--- src/Components/NewCostOfLivingComponent.js
+++ src/Components/NewCostOfLivingComponent.js
@@ -80,91 +80,12 @@ class NewCostOfLivingComponent extends React.Component {
 		super(props);
 		const dataSet = require('../data/cost_of_living_indices.json');
 
-		if (this.props.newCitySlug) {		
-			this.state = {
-				listOfSalaries: '',
-				position: '',
-				currentCurrency: dataSet[this.props.currentCity].currency_type,
-				targetCurrency: dataSet[this.props.newCity].currency_type,
-				currencyResponseRates: '',
-				salary: '',
-				onLastPage: true,
-				openSpotAHomeDetails: false
-			}
-		} else {
-			this.state = {
-				currentCurrency: dataSet[this.props.currentCity].currency_type,
-				targetCurrency: dataSet[this.props.newCity].currency_type,
-				currencyResponseRates: '',
-				lastPage: true,
-				openSpotAHomeDetails: false
-			}
+		this.state = {
+			currentCurrency: dataSet[this.props.currentCity].currency_type,
+			targetCurrency: dataSet[this.props.newCity].currency_type,
+			onLastPage: true,
+			openSpotAHomeDetails: false
 		}
-    this.fetchCurrencyResponseRates = this.fetchCurrencyResponseRates.bind(this);
-		this.changePosition = this.changePosition.bind(this);
-	}
-
-	componentDidMount () {
-    Promise.resolve(this.fetchCurrencyResponseRates()).then(() => {
-    	if (this.props.newCitySlug) {
-      	fetch('https://api.teleport.org/api/urban_areas/slug:'+this.props.newCitySlug+'/salaries/')
-        .then((response) => {
-      		if (!response.ok) {
-          		throw Error('Something went wrong retreiving city information :(');
-          	}
-          	return response.json();
-      	})
-        .then((responseData) => {
-        	let randomObject = responseData.salaries[Math.floor(Math.random()*responseData.salaries.length)];
-   			  let randomPosition = randomObject.job.title;
-   			  let randomSalary = randomObject.salary_percentiles.percentile_50;
-          	
-        	let configuredSalary = (randomSalary/this.state.currencyResponseRates['USD'])*this.state.currencyResponseRates[this.state.targetCurrency];
-        	let roundedConfiguredSalary = (Math.round(configuredSalary/100)*100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          	
-        	this.setState({
-        		listOfSalaries: responseData,
-          	position: randomPosition,
-          	salary: roundedConfiguredSalary
-          });
-		    })
-        .catch((error) => {
-        		console.log(error);
-      	});
-      }
-    });    				
-	}
-
-	changePosition () {
-		let randomObject = this.state.listOfSalaries.salaries[Math.floor(Math.random()*this.state.listOfSalaries.salaries.length)];
-		let randomPosition = randomObject.job.title;
-		let randomSalary = randomObject.salary_percentiles.percentile_50;
-
-		let configuredSalary = (randomSalary/this.state.currencyResponseRates['USD'])*this.state.currencyResponseRates[this.state.targetCurrency];
-		let roundedConfiguredSalary = (Math.round(configuredSalary/100)*100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-		this.setState({
-			position: randomPosition,
-			salary: roundedConfiguredSalary
-		})
-	}
-
-	fetchCurrencyResponseRates () {
-		fetch('https://openexchangerates.org/api/latest.json?app_id=0cc840f2153c4d378b1a2687918435e7')
-      .then((response) => {
-    		if (!response.ok) {
-        		throw Error('Something went wrong retreiving currency information :(');
-        }
-        	return response.json();
-    	})
-      .then((responseData) => {
-  			this.setState({
-				  currencyResponseRates: responseData.rates
-			  });
-			})
-			.catch((error) => {
-      	console.log(error);
-    	});
 	}
 
 	refreshCity = (e) => {
@@ -172,25 +93,14 @@ class NewCostOfLivingComponent extends React.Component {
 	}
 
 	isASpotAHomeCity = () => {
-		if (this.props.newCity === 'Madrid, Spain' 
-			|| this.props.newCity === 'Barcelona, Spain' 
-			|| this.props.newCity === 'Bilbao, Spain'
-			|| this.props.newCity === 'Valencia, Spain'
-			|| this.props.newCity === 'Granada, Spain'
-			|| this.props.newCity === 'Seville, Spain'
-			|| this.props.newCity === 'Brussels, Belgium'
-			|| this.props.newCity === 'London, United Kingdom'
-			|| this.props.newCity === 'Rome, Italy'
-			|| this.props.newCity === 'Milan, Italy'
-			|| this.props.newCity === 'Paris, France'
-			|| this.props.newCity === 'Lyon, France'
-			|| this.props.newCity === 'Dublin, Ireland'
-			|| this.props.newCity === 'Dubai, United Arab Emirates'
-			|| this.props.newCity === 'Berlin, Germany'
-			|| this.props.newCity === 'Vienna, Austria'
-		) {
-			return true;
-		}
+    const cities = [
+      'Madrid, Spain', 'Barcelona, Spain', 'Bilbao, Spain', 'Valencia, Spain',
+      'Granada, Spain', 'Seville, Spain', 'Brussels, Belgium',
+      'London, United Kingdom', 'Rome, Italy', 'Milan, Italy', 
+      'Paris, France', 'Lyon, France', 'Dublin, Ireland', 
+      'Dubai, United Arab Emirates', 'Berlin, Germany', 'Vienna, Austria'
+    ]
+		return (cities.indexOf(this.props.newCity) >= 0);
 	}
 
 	openSpotAHomeDetails = () => {
@@ -260,8 +170,12 @@ class NewCostOfLivingComponent extends React.Component {
 						  />
 					</div>
 
-						{(this.props.newCitySlug) && <RandomPosition />}
-
+						{(this.props.newCitySlug) &&
+							<RandomPosition 
+							  targetCurrency={this.state.targetCurrency}
+                newCity={this.props.newCity}
+                newCitySlug={this.props.newCitySlug}
+						  />}
 					</div>
 				</div>}
 
@@ -273,5 +187,4 @@ class NewCostOfLivingComponent extends React.Component {
 		);
 	}
 }
-
 export default NewCostOfLivingComponent;
--- src/Components/SubComponents/RandomPosition.js
+++ src/Components/SubComponents/RandomPosition.js
@@ -17,14 +17,84 @@ let super_script = {
   verticalAlign: 'super',
   fontSize: '15px'
 }
-class RandomPosition extends React.component {
+class RandomPosition extends React.Component {
   constructor(props){
     super(props);
-    this.state ={
+    this.state = {
       listOfSalaries: '',
       position: '',
-      salary: ''
+      salary: '',
+      currencyResponseRates: '',
     }
+    this.fetchCurrencyResponseRates = this.fetchCurrencyResponseRates.bind(this);
+    this.fetchPositionData = this.fetchPositionData.bind(this);
+    this.changePosition = this.changePosition.bind(this);
+  }
+
+  componentDidMount () {
+   this.fetchCurrencyResponseRates();          
+  }
+
+  changePosition () {
+    let randomObject = this.state.listOfSalaries.salaries[Math.floor(Math.random()*this.state.listOfSalaries.salaries.length)];
+    let randomPosition = randomObject.job.title;
+    let randomSalary = randomObject.salary_percentiles.percentile_50;
+
+    let configuredSalary = (randomSalary/this.state.currencyResponseRates['USD'])*this.state.currencyResponseRates[this.props.targetCurrency];
+    let roundedConfiguredSalary = (Math.round(configuredSalary/100)*100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    this.setState({
+      position: randomPosition,
+      salary: roundedConfiguredSalary
+    })
+  }
+
+  fetchCurrencyResponseRates () {
+    fetch('https://openexchangerates.org/api/latest.json?app_id=0cc840f2153c4d378b1a2687918435e7')
+      .then((response) => {
+        if (!response.ok) {
+            throw Error('Something went wrong retreiving currency information :(');
+        }
+          return response.json();
+      })
+      .then((responseData) => {
+        this.setState({
+          currencyResponseRates: responseData.rates
+        });
+      })
+      .then(() => {
+        this.fetchPositionData();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  fetchPositionData () {
+    fetch('https://api.teleport.org/api/urban_areas/slug:'+this.props.newCitySlug+'/salaries/')
+      .then((response) => {
+        if (!response.ok) {
+            throw Error('Something went wrong retreiving city information :(');
+          }
+          return response.json();
+      })
+      .then((responseData) => {
+        let randomObject = responseData.salaries[Math.floor(Math.random()*responseData.salaries.length)];
+        let randomPosition = randomObject.job.title;
+        let randomSalary = randomObject.salary_percentiles.percentile_50;
+          
+        let configuredSalary = (randomSalary/this.state.currencyResponseRates['USD'])*this.state.currencyResponseRates[this.props.targetCurrency];
+        let roundedConfiguredSalary = (Math.round(configuredSalary/100)*100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        
+        this.setState({
+          listOfSalaries: responseData,
+          position: randomPosition,
+          salary: roundedConfiguredSalary
+        });
+      })
+      .catch((error) => {
+          console.log(error);
+      });
   }
   render() {
     return (
@@ -35,7 +105,7 @@ class RandomPosition extends React.component {
             data-tooltip='Click for another occupation!'> {this.state.position} 
               <span style={super_script}> <i className="fa fa-user" aria-hidden="true"></i> </span>&nbsp;
             </span> 
-          in {this.props.newCity} is around <span className='random-salary'>{this.state.targetCurrency} {this.state.salary}</span></p>
+          in {this.props.newCity} is around <span className='random-salary'>{this.props.targetCurrency} {this.state.salary}</span></p>
         </div>
       </div>
     )
